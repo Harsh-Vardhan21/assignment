6a4dbf2d2f1e2436336a9a2632c2987fb2e65dc0
Harsh Vardhan <harsh.1vardhan123@gmail.com>
2024-07-03 20:15:29 +0530

    fixes

--- server/server.js
+++ server/server.js
@@ -1,17 +1,40 @@
+// Import modules using ES Modules syntax
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 
+// Load environment variables from .env file
 dotenv.config();
+
+// Connect to MongoDB database
 connectDB();
 
+// Create an instance of Express application
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use('/api/users', userRoutes);
+// Middleware
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Parse incoming JSON requests
 
+// Serve static files from the React app
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+// API routes
+app.use('/api/users', userRoutes); // Mount user routes
+
+// Handle other requests that don't match the API route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+// Define the port to listen on
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
