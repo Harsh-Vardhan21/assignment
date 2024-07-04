d5f5737e958e5d9ccff1af08b0cbfaf24b6eb91e
Harsh Vardhan <harsh.1vardhan123@gmail.com>
2024-07-04 23:49:45 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -36,7 +36,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await axios.post('http://localhost:5000/api/users/signup', {
+      const response = await axios.post('https://assignment-himu.onrender.com/api/users/signup', {
         fullName,
         email,
         password
